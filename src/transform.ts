c64012c3fecc01e8e356ce81cfc5f754e610721d
rhf007 <rahofahazem512@gmail.com>
2026-05-23 13:33:04 +0300

    update yearFounded

--- src/transform.ts
+++ src/transform.ts
@@ -6,9 +6,32 @@ const minYear = 1600;
 
 export async function transformBrands() {
   const db = mongoose.connection.db;
-  if (!db) throw new Error("Database connection not established");
+  if (!db) throw new Error("Database connection failed");
 
   const raw = await db.collection("brands").find({}).toArray();
 
-  console.log(raw);
+  for (const doc of raw) {
+    console.log(doc);
+    // fix yearFounded issues
+    let yearFounded = doc.yearCreated ?? doc.yearsFounded ?? minYear;
+    yearFounded = Number(yearFounded);
+    if (isNaN(yearFounded)) {
+      yearFounded = minYear;
+    }
+
+    const rightDoc = { yearFounded };
+    await Brand.findByIdAndUpdate(
+      doc._id,
+      {
+        $set: rightDoc,
+        $unset: { yearsFounded: "", yearCreated: "" },
+      },
+      // $unset doesnt work when strict is true
+      { strict: false },
+    );
+
+    console.log("===============");
+    console.log(`Transformed: ${doc._id}`);
+    console.log(doc);
+  }
 }
